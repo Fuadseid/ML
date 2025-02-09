9d974b69a955ff7090554240ad634238d6fd0ca2
Fuadseid <fuaddbus@gmail.com>
2025-02-09 11:29:34 +0300

    update something

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -148,7 +148,7 @@ function Form() {
           {loadingButton ? (
             <div className="flex items-center justify-center">
               <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-white"></div>
-              <span className="ml-2">Predicting...</span>
+              <span className="ml-2">Predicting....</span>
             </div>
           ) : (
             "Predict"
